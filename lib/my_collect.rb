064466725f8ce50736e90a34f8eed8bc85c37002
Becky Rosa <becky_m_rosa@yahoo.com>
2021-02-22 02:20:54 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,10 +2,10 @@ def my_collect(array)
   a = 0
   collection = []
    array.each do |e|
-    yield(e)
+    collection <<  yield(e)
   a += 1
 end
-array
+collection
 end
 
 my_collect(["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]) { |word| "#{word}" }
